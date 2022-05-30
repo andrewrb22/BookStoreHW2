e892ff866795686f4d961787a3834fc9dffdec34
Andrew Rodriguez <andrewrb22@gmail.com>
2022-05-30 13:44:42 -0400

    Cleaning code

--- Tester.java
+++ Tester.java
@@ -32,6 +32,6 @@
         b1.setPublisher("rty");
         Book b2 = new Book("654321", 99.12);
         b2.setPublisher("xyz");
-        System.out.println(b1.toString() + "    " + 
+        System.out.println( b1.toString() + "    " + 
         b2.toString());
         }}
